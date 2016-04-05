e2881ece7944b363767a8fa67e3d867147eb0a5a
Desmond dai <desmonddai583@gmail.com>
2016-04-05 18:36:49 +0800

    init commit

--- main.go
+++ main.go
@@ -1,16 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"github.com/mitchellh/mapstructure"
 	"net/http"
-	"time"
 )
-
-type Message struct {
-	Name string      `json:"name"`
-	Data interface{} `json:"data"`
-}
 
 type Channel struct {
 	Id   string `json:"id"`
@@ -20,7 +12,7 @@ type Channel struct {
 func main() {
 	router := NewRouter()
 
-	router.Handle("channel add", addChannel)
+	// router.Handle("channel add", addChannel)
 
 	http.Handle("/", router)
 	http.ListenAndServe(":4000", nil)
